e3f4ef018821a07262b163877b02f11bf9ae3b81
martin-pio <63728713+martin-pio@users.noreply.github.com>
2022-04-06 23:45:44 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import "./styles.css";
+
+ReactDOM.render(<App />, document.getElementById("root"));
+
+//https://www.freepik.com/premium-psd/it-company-website-landing-page-template_16501699.htm#query=website&position=39&from_view=search
